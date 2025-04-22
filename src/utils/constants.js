1d78a89f74ff05d5e91b4033a93b1a17690b4b46
Robert Hatcher <Robhatcher14@gmail.com>
2025-04-21 19:37:54 -0500

    Updates

--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const APIkey="28d898fcf3ef4c1d91cb0e68b6a55016";
-
-export const newsApiBaseUrl = process.env.NODE_ENV === "production" 
-  ? "https://nomoreparties.co/news/v2/everything"
-  : "https://newsapi.org/v2/everything";
